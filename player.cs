e7e0059006cf01cd7a17a2532ede440a069d462b
Eric Zhang <ericbkzhang@gmail.com>
2024-03-13 14:52:01 -0400

    Started player script
    Renamed the script from player.cs to Player.cs
    It uses the default Godot code

--- player.cs
+++ player.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class player : CharacterBody2D
+public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
@@ -15,11 +15,15 @@ public partial class player : CharacterBody2D
 
 		// Add the gravity.
 		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+		{
+			velocity.Y += gravity * (float) delta;
+		}
 
 		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
 			velocity.Y = JumpVelocity;
+		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
