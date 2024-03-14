6ba5682b0ebe67fec804a5cbd477e6da4b0d77d0
Eric Zhang <ericbkzhang@gmail.com>
2024-03-14 16:47:20 -0400

    Enabled ground collision and adjusted player movement
    The player now gradually accelerates to full horizontal speed rather than instantly moving at full speed

--- player.cs
+++ player.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
+	public const float Speed = 200.0f;
 	public const float JumpVelocity = -400.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -20,21 +20,23 @@ public partial class Player : CharacterBody2D
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("ui_up ") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		float direction = Input.GetAxis("ui_left", "ui_right");
+		if (direction != 0)
 		{
-			velocity.X = direction.X * Speed;
+			
+			velocity.X = Mathf.MoveToward(Velocity.X, direction * Speed, 10);
 		}
+		
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, 10);
 		}
 
 		Velocity = velocity;
