1f324a25518d4fc3945fbe3fee36643d7c053021
Eric2422 <ericbkzhang@gmail.com>
2024-03-21 00:06:43 -0400

    Add _Input() method to main.cs to exit the game upon pressing Esc

--- main.cs
+++ main.cs
@@ -3,6 +3,14 @@ using System;
 
 public partial class main : Node2D
 {
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("ui_cancel"))
+		{
+			GetTree().Quit();
+		}
+	}
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
