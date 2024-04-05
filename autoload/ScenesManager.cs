db0bf6f3d8f4dbffeea3784b27fe777a705bb3a4
Eric2422 <ericbkzhang@gmail.com>
2024-04-04 20:37:13 -0400

    Merging

--- autoload/ScenesManager.cs
+++ autoload/ScenesManager.cs
@@ -3,52 +3,52 @@ using System.Collections.Generic;
 
 public partial class ScenesManager : Node
 {
-    public Node CurrentScene { get; set; }
+	public Node CurrentScene { get; set; }
 
-    public override void _Ready()
-    {
-        Viewport root = GetTree().Root;
-        CurrentScene = root.GetChild(root.GetChildCount() - 1);
-    }
+	public override void _Ready()
+	{
+		Viewport root = GetTree().Root;
+		CurrentScene = root.GetChild(root.GetChildCount() - 1);
+	}
 
-    /// <summary>
-    /// Changes the scene from the current scene to the next scene.
-    /// Transfers the player from this scene to the next.
-    /// </summary>
-    /// <param name="sceneName">The next scene to be loaded.</param>
-    public void ChangeScene(string sceneName)
-    {
-        // Wait until it is safe to change scenes
-        // Otherwise, it may interrupt some code that is still executing
-        CallDeferred(MethodName.DeferredChangeScene, sceneName);
-    }
+	/// <summary>
+	/// Changes the scene from the current scene to the next scene.
+	/// Transfers the player from this scene to the next.
+	/// </summary>
+	/// <param name="sceneName">The next scene to be loaded.</param>
+	public void ChangeScene(string sceneName)
+	{
+		// Wait until it is safe to change scenes
+		// Otherwise, it may interrupt some code that is still executing
+		CallDeferred(MethodName.DeferredChangeScene, sceneName);
+	}
 
-    public void DeferredChangeScene(string sceneName)
-    {
-        // Remove the player from the current scene
-        Player player = CurrentScene.GetNode<Player>("Player");
-        CurrentScene.RemoveChild(player);
+	public void DeferredChangeScene(string sceneName)
+	{
+		// Remove the player from the current scene
+		Player player = CurrentScene.GetNode<Player>("Player");
+		CurrentScene.RemoveChild(player);
 
-        // It is now safe to remove the current scene.
-        string oldSceneName = CurrentScene.Name;
-        CurrentScene.Free();
+		// It is now safe to remove the current scene.
+		string oldSceneName = CurrentScene.Name;
+		CurrentScene.Free();
 
-        GD.Print($"sceneName: {sceneName}");
+		GD.Print($"sceneName: {sceneName}");
 
-        // Load a new scene.
-        var nextScene = GD.Load<PackedScene>($"res://{sceneName}/{sceneName}.tscn").Instantiate();
-        CurrentScene = nextScene;
+		// Load a new scene.
+		var nextScene = GD.Load<PackedScene>($"res://{sceneName}/{sceneName}.tscn").Instantiate();
+		CurrentScene = nextScene;
 
-        // Add the player to the scene as a child
-        CurrentScene.AddChild(player);
+		// Add the player to the scene as a child
+		CurrentScene.AddChild(player);
 
-        // Reposition the player to the appropriate door.
-        player.Position = CurrentScene.GetNode<Node2D>($"{oldSceneName}Door").Position;
+		// Reposition the player to the appropriate door.
+		player.Position = CurrentScene.GetNode<Node2D>($"{oldSceneName}Door").Position;
 
-        // Add it to the active scene, as child of root.
-        GetTree().Root.AddChild(CurrentScene);
+		// Add it to the active scene, as child of root.
+		GetTree().Root.AddChild(CurrentScene);
 
-        // Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
-        GetTree().CurrentScene = CurrentScene;
-    }
+		// Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
+		GetTree().CurrentScene = CurrentScene;
+	}
 }
