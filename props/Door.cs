b58bc55c5f6f9a2e3ff76e42874b0c53ea4f4b83
Eric Zhang <ericbkzhang@gmail.com>
2024-04-15 14:59:05 -0400

    Add an export to door

--- props/Door.cs
+++ props/Door.cs
@@ -2,6 +2,10 @@ using Godot;
 
 public partial class Door : Interactable
 {
+	// The room that this door leads to
+	[Export]
+	public string targetRoom;
+
 	public override void _Ready()
 	{
 		base._Ready();
